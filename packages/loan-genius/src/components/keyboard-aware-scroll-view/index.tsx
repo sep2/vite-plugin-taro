eed26258464c8eca596db4688f1b21cb8fc1d764
felix <i@lcz.link>
2026-06-20 18:40:14 +0800

    Fix import order in KeyboardAwareScrollView and remove redundant class property

--- packages/loan-genius/src/components/keyboard-aware-scroll-view/index.tsx
+++ packages/loan-genius/src/components/keyboard-aware-scroll-view/index.tsx
@@ -5,13 +5,13 @@
  * @Last Modified by: qiuz
  */
 
-import clsx from 'clsx'
 import { ScrollView } from 'virtual:taro/components'
+import clsx from 'clsx'
 
 function TaroKeyboardAwareScrollView(props: any) {
     const { className = '', children } = props
     return (
-        <ScrollView scrollY className={clsx('flex-1 overflow-hidden', className)}>
+        <ScrollView scrollY className={clsx('flex-1', className)}>
             {children}
         </ScrollView>
     )
