692c09618fa1d7f46143df34abbe435b09d19b9a
felix <i@lcz.link>
2026-06-21 01:06:26 +0800

    Update Taro import path in Loan Genius App component

--- packages/loan-genius/src/app.ts
+++ packages/loan-genius/src/app.ts
@@ -1,5 +1,5 @@
-import Taro from 'virtual:taro'
 import type { PropsWithChildren } from 'react'
+import Taro from 'vite-plugin-taro/taro'
 import './app.css'
 
 function App({ children }: PropsWithChildren) {
