ac680b08ce65b2d965331a4a55e83a22991321ff
felix <i@lcz.link>
2026-06-21 14:36:46 +0800

    Refactor `App` component: adjust import order, update console log message, and fix README example

--- packages/loan-genius/src/app.ts
+++ packages/loan-genius/src/app.ts
@@ -1,10 +1,10 @@
-import type { PropsWithChildren } from 'react'
 import Taro from 'virtual:taro/api'
+import type { PropsWithChildren } from 'react'
 import './app.css'
 
 function App({ children }: PropsWithChildren) {
     Taro.useLaunch(() => {
-        console.log('App Launch')
+        console.log('App launched')
     })
 
     return children
