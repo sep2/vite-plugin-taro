26ec15d13727a4856f79d853e2b024d13a7cc76f
felix <i@lcz.link>
2026-07-11 14:20:36 +0800

    refactor: remove unused `isProd` constant from Vite Taro constants file

--- packages/vite-plugin-taro/src/vite/constants.ts
+++ packages/vite-plugin-taro/src/vite/constants.ts
@@ -2,8 +2,6 @@ import { createRequire } from 'node:module'
 import path from 'node:path'
 import { toImportPath } from './utils.ts'
 
-export const isProd = process.env.NODE_ENV === 'production'
-
 export const nodeRequire = createRequire(import.meta.url)
 
 export const packageRoot = path.dirname(nodeRequire.resolve('vite-plugin-taro/package.json'))
