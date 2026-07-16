527c0a0fef958c84be5d1925fec259d2cd1fe0aa
felix <i@lcz.link>
2026-07-17 05:40:47 +0800

    refactor

---
+++ packages/vite-plugin-taro/src/runtime/wx/app.ts
@@ -0,0 +1,5 @@
+import './bootstrap.ts'
+
+declare const __VITE_PLUGIN_TARO_APP_CONFIG__: Record<string, unknown>
+
+App({ config: __VITE_PLUGIN_TARO_APP_CONFIG__ })
---
+++ packages/vite-plugin-taro/src/runtime/wx/wechat.d.ts
@@ -0,0 +1,2 @@
+/** Registers the native WeChat Mini Program application. */
+declare function App(options: Record<string, unknown>): void
