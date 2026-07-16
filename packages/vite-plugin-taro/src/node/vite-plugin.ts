87a053f5288576d330941f38582d898092c0edd4
felix <i@lcz.link>
2026-07-16 22:32:36 +0800

    feat: add client-side plugin integration to vitePluginTaro

--- packages/vite-plugin-taro/src/node/vite-plugin.ts
+++ packages/vite-plugin-taro/src/node/vite-plugin.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
 import type { PluginOption } from 'vite'
 import type { VitePluginTaroOptions } from '../options.ts'
+import { createClientTaroPlugin } from './plugins/client-taro.ts'
 import { createWxTargetPlugin } from './target/wx/plugin.ts'
 
 export default function vitePluginTaro(options: VitePluginTaroOptions): PluginOption[] {
-    return [...react(), ...(options.target === 'wx' ? [createWxTargetPlugin()] : [])]
+    return [createClientTaroPlugin(), ...react(), ...(options.target === 'wx' ? [createWxTargetPlugin(options)] : [])]
 }
