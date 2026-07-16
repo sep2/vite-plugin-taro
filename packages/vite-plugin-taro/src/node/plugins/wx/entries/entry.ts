19971704eaa4c8c6f2041ffd032a593d2c42e58e
felix <i@lcz.link>
2026-07-17 04:44:17 +0800

    refactor: update WX entry module IDs for consistency with new modular naming convention

--- packages/vite-plugin-taro/src/node/plugins/wx/entries/entry.ts
+++ packages/vite-plugin-taro/src/node/plugins/wx/entries/entry.ts
@@ -1,12 +1,12 @@
 import { toViteFileImportPath } from '../../../utils/modules.ts'
 import { resolvePackageFile } from '../../../utils/packages.ts'
 
-export const appEntryId = 'virtual:vite-plugin-taro/wx/app'
+export const appEntryId = '\0vpt:app'
 export const taroBridgeImportPath = toViteFileImportPath(resolvePackageFile('dist/runtime/wx/taro-bridge.js'))
 
-const pageEntryIdPrefix = 'virtual:vite-plugin-taro/wx/page/'
+const pageEntryIdPrefix = '\0vpt:page/'
 
 /** Converts a page path to its entry module ID. */
-export function pagePathToEntryId(pagePath: string): string {
+export function pagePathToEntryId(pagePath: string): `\0${string}` {
     return `${pageEntryIdPrefix}${pagePath}`
 }
