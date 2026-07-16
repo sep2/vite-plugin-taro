184dc5bc7e06356ed01460d690bf6318e97f7347
felix <i@lcz.link>
2026-07-17 03:53:40 +0800

    feat: support WX bootstrap chunk rendering in postRenderChunk function

--- packages/vite-plugin-taro/src/node/plugins/wx/render/post-render-chunk.ts
+++ packages/vite-plugin-taro/src/node/plugins/wx/render/post-render-chunk.ts
@@ -2,13 +2,19 @@ import { type PluginTarget, transformSync } from '@babel/core'
 import transformDynamicImport from '@babel/plugin-transform-dynamic-import'
 import transformModulesSystemjs from '@babel/plugin-transform-modules-systemjs'
 import type { Rolldown } from 'vite'
+import { wxBootstrapEntryName } from '../virtual/virtual-modules.ts'
+import { renderBootstrap } from './render-bootstrap.ts'
 import { systemRegisterCapsulePlugin } from './system-register.ts'
 
-/** Converts one final Rolldown ESM chunk into an inert native System registration capsule. */
+/** Converts one final Rolldown chunk into its native WX representation. */
 export function postRenderChunk(
     code: string,
-    chunk: Pick<Rolldown.RenderedChunk, 'fileName'>
+    chunk: Pick<Rolldown.RenderedChunk, 'fileName' | 'isEntry' | 'name'>
 ): { code: string; map: Rolldown.ExistingRawSourceMap } {
+    if (chunk.isEntry && chunk.name === wxBootstrapEntryName) {
+        return renderBootstrap(code, chunk.fileName)
+    }
+
     const capsule = transformSync(code, {
         babelrc: false,
         compact: false,
