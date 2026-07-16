117f1179e86f959aa08f8f8ee8ff2b9f1186e31e
felix <i@lcz.link>
2026-07-17 05:40:55 +0800

    refactor

--- packages/vite-plugin-taro/src/runtime/wx/bootstrap.ts
+++ packages/vite-plugin-taro/src/runtime/wx/bootstrap.ts
@@ -1,5 +1,5 @@
 import 'systemjs/s.js'
 
-declare function __VITE_PLUGIN_TARO_NATIVE_REQUIRE__(id: '../transport.js'): Pick<System.Loader, 'instantiate'>
+declare function __VITE_PLUGIN_TARO_NATIVE_REQUIRE__(id: './transport.js'): Pick<System.Loader, 'instantiate'>
 
-System.instantiate = __VITE_PLUGIN_TARO_NATIVE_REQUIRE__('../transport.js').instantiate
+System.instantiate = __VITE_PLUGIN_TARO_NATIVE_REQUIRE__('./transport.js').instantiate
