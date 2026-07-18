e2b0c3703369a02858d070d4cd26cb4453afe01c
felix <i@lcz.link>
2026-07-19 00:35:37 +0800

    refactor: relocate `serialized-task-queue` utility to `utils` folder, update imports

--- packages/vite-plugin-taro/src/node/plugins/wx/dev/hmr-server.ts
+++ packages/vite-plugin-taro/src/node/plugins/wx/dev/hmr-server.ts
@@ -5,7 +5,7 @@ import type { InputOptions, OutputOptions } from 'rolldown'
 import { type DevEngine, dev, viteReporterPlugin } from 'rolldown/experimental'
 import type { ViteDevServer } from 'vite'
 import type { VitePluginTaroOptions } from '../../../../options.ts'
-import { SerializedTaskQueue } from './serialized-task-queue.ts'
+import { SerializedTaskQueue } from '../../../utils/serialized-task-queue.ts'
 import { controlFileName, updateFileName } from './support.ts'
 
 // Rolldown's generated development modules reference this lexical binding. Native and SystemJS-rendered chunks cannot
