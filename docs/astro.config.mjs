9ee215fae1bfb2ec3fb849725ac38799d04070e5
felix <i@lcz.link>
2026-08-06 15:45:16 +0800

    docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -35,7 +35,8 @@ export default defineConfig({
                         { label: '快速开始', slug: 'guides/quick-start' },
                         { label: '全自动分包', slug: 'guides/automatic-subpackages' },
                         { label: '开发者工具热更新', slug: 'guides/hot-module-replacement' },
-                        { label: 'Skyline 模式', slug: 'guides/skyline-mode' }
+                        { label: 'Skyline 模式', slug: 'guides/skyline-mode' },
+                        { label: '从 Taro CLI 迁移', slug: 'guides/migrate-from-taro' }
                     ]
                 },
                 {
