bf61d0d48be9c5a939f97659a70c9f3ac24d3381
felix <i@lcz.link>
2026-08-06 11:37:32 +0800

    docs: add Starlight documentation site

---
+++ docs/astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://sep2.github.io',
+    base: '/vite-plugin-taro',
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' }
+                    ]
+                },
+                {
+                    label: 'Reference',
+                    items: [{ autogenerate: { directory: 'reference' } }]
+                }
+            ]
+        })
+    ]
+})
