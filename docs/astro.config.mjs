6f8050985e41f4399a143e924e1588ea7d57fbd6
felix <i@lcz.link>
2026-08-11 22:05:55 +0800

    styles docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -33,6 +33,7 @@ export default defineConfig({
                     label: '开始使用',
                     items: [
                         { label: '快速开始', slug: 'guides/quick-start' },
+                        { label: '样式', slug: 'guides/styles' },
                         { label: '全自动分包', slug: 'guides/automatic-subpackages' },
                         { label: '开发者工具热更新', slug: 'guides/hot-module-replacement' },
                         { label: '微信原生组件', slug: 'guides/native-components' },
